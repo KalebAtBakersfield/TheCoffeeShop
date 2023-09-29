58707457f5aadb74eb204e7a1697682374059c33
KalebAtBakersfield <146099804+KalebAtBakersfield@users.noreply.github.com>
2023-09-29 08:31:53 -0500

    trying to un js things

--- index.js
+++ index.js
@@ -3,20 +3,6 @@ const app = Vue.createApp({
         return {
             today: new Date(2024,9),
             openDate: new Date(2023, 9, 10),
-
-
-            items: {
-                regular: {
-                    name: "Regular",
-                    price: 2,
-                    description: "Regular cuppa joe.",
-                },
-                specialty: {
-                    name: "Specialty",
-                    price: 3,
-                    description: "Periodically we will have specialty coffee. We will let you know when it will be avaliable.",
-                },
-            }
         }
     },
     methods: {
